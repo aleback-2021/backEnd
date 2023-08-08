b34052023478c0359684084da6c7be40643325e0
alejandro.orellano <aleback.2021@gmail.com>
2023-08-07 22:24:15 -0300

    Segundo Desafio

---
+++ productManager.js
@@ -0,0 +1,72 @@
+import {promises as fs} from "fs"
+
+
+
+class ProductManager 
+{
+    constructor(){
+        this.patch = "./productos.txt"
+        this.products= []
+    }
+
+    static id = 0 
+
+    addProduct = async (title, description, price, thumbnail, code, stock) =>{
+
+        this.id++;
+
+        let newProduct = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock,
+            id: this.id,
+        }
+
+        this.products.push(newProduct)
+
+        await fs.writeFile(this.patch, JSON.stringify(this.products));
+    };
+
+    readProducts = async () => {
+        let respuesta = await fs.readFile(this.patch, "utf-8")
+        return JSON.parse(respuesta)
+    }
+    
+    getProducts = async () => {
+        let respuesta2 = await this.readProducts()
+        return console.log(respuesta2)
+    }
+
+    getProductsById = async (id) => {
+        let respuesta3 = await this.readProducts()
+        if(!respuesta3.find(product => product.id === id)){
+            console.log("No se encuentra el producto")
+        } else{
+            console.log(respuesta3.find(product => product.id === id));
+        }
+    }
+
+    deleteProductsById = async (id) => {
+        let respuesta3 = await this.readProducts()
+        let productFilter = respuesta3.filter(products => products.id != id)
+        await fs.writeFile(this.patch, JSON.stringify(productFilter));
+        
+        console.log("Se ha encontrado el producto")
+    }
+
+    updateProducts = async ({id, ...producto}) => {
+        await this.deleteProductsById(id);
+        let oldProduct = await this.readProducts()
+        let modifiedProduct = [{ ...producto, id }, ...oldProduct];
+        await fs.writeFile(this.patch, JSON.stringify(modifiedProduct));
+      
+    }
+
+}
+
+const produc = new productManager();
+
+produc.addProduct("producto prueba11", "Este es un producto prueba", 350, "Sin imagen", "abc123", 25);
